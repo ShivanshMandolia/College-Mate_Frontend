bb2a8a31ee59ca3319f9ae7463754741bb7b4bb4
ShivanshMandolia <shivanshmandolia2005@gmail.com>
2025-05-17 18:22:05 +0530

    Add default export to addplacement component

---
+++ src/pages/superadmin/addplacement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AddPlacement = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">Add Placement</h1>
+      {/* Add placement form will go here */}
+    </div>
+  )
+}
+
+export default AddPlacement
