d0a27d41905140cbe6fbd9e9ccda112d3e824dc0
ShivanshMandolia <shivanshmandolia2005@gmail.com>
2025-05-17 18:23:37 +0530

    Add default export to complaint page component

---
+++ src/pages/superadmin/complanetpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ComplaintPage = () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">Complaints Management</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p className="text-gray-600">Complaints list will be displayed here</p>
+      </div>
+    </div>
+  );
+};
+
+export default ComplaintPage;
