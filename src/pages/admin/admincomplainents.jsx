f4cb52fb270a8658038b389ee39ec3f57d1d6cf7
ShivanshMandolia <shivanshmandolia2005@gmail.com>
2025-05-17 18:22:20 +0530

    Add default export to admin complaints page

---
+++ src/pages/admin/admincomplainents.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AdminComplaints = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">Admin Complaints Dashboard</h1>
+      <div className="bg-white rounded-lg shadow p-6">
+        <p>Complaints management interface will be implemented here</p>
+      </div>
+    </div>
+  );
+};
+
+export default AdminComplaints;
