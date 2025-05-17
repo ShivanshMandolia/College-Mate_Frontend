fa5bb579b4574659a6d8c530e749ce3ebea1cef9
ShivanshMandolia <shivanshmandolia2005@gmail.com>
2025-05-17 13:55:01 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import { useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/auth/LoginPage';
-import RegisterPage from './pages/auth/RegisterPage';
-import LostFoundPage from './pages/LostFoundPage';
-import ComplaintPage from './pages/ComplaintPage';
-import PlacementPage from './pages/PlacementPage';
-import EventPage from './pages/EventPage';
-import NotFoundPage from './pages/NotFoundPage';
-import DashboardPage from './pages/DashboardPage';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import Layout from './components/layout/Layout';
-import { useAuth } from './hooks/useAuth';
+
 
 function App() {
   const location = useLocation();
-  const { isAuthenticated } = useAuth();
 
   // Scroll to top on route change
   useEffect(() => {
@@ -23,27 +10,9 @@ function App() {
   }, [location.pathname]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Public routes */}
-        <Route index element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-
-        {/* Protected routes */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="lost-found" element={<LostFoundPage />} />
-          <Route path="complaints" element={<ComplaintPage />} />
-          <Route path="placements" element={<PlacementPage />} />
-          <Route path="events" element={<EventPage />} />
-        </Route>
-
-        {/* 404 route */}
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  );
+  
+     
+  )
 }
 
 export default App;
