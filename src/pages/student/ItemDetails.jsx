cf75187bf4b543ccc8c3c1e795c42883d11340a0
ShivanshMandolia <shivanshmandolia2005@gmail.com>
2025-05-18 18:08:55 +0530

    Fix incorrect import path

--- src/pages/student/ItemDetails.jsx
+++ src/pages/student/ItemDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
-import { useGetItemDetailsQuery } from '../../../features/lostFound/lostFoundApiSlice';
+import { useGetItemDetailsQuery } from '../../features/lostFound/lostFoundApiSlice';
 import { Calendar, MapPin, Tag, AlertCircle, ArrowLeft, Eye } from 'lucide-react';
 
 const ItemDetails = () => {
